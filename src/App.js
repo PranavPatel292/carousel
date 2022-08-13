a32cfe1bc9ef69c96189bf1a44108897b2dfc56f
= <pranav.patel@exodigital.com.au>
2022-08-14 09:12:49 +1000

    added some of the animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 
 function App({ classname, numberOfEle = 4 }) {
   const widthDiv = window.innerWidth / numberOfEle;
+
+  const [next, setNext] = useState(false);
 
   const childern = [
     "hello",
@@ -13,22 +16,55 @@ function App({ classname, numberOfEle = 4 }) {
     "hello",
     "hello",
     "hello",
+    "hello",
+    "hello",
+    "hello",
   ];
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setNext(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [next]);
+
+  const onNextClick = () => {
+    setNext(true);
+  };
   return (
-    <div className="flex">
-      <div className="flex space-x-4">
-        {childern?.map((item, index) => {
-          return (
-            <div
-              key={index}
-              style={{ width: `${widthDiv}px` }}
-              className={`h-auto bg-red-500 ${classname}`}
-            >
-              <h1>{item}</h1>
-            </div>
-          );
-        })}
+    <div>
+      <div className="flex">
+        <div
+          className={`w-full ${
+            next ? "translate-x-[-200%]" : "translate-x-[-100%]"
+          }`}
+        >
+          <div className="flex absolute ">
+            {childern?.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{ width: `${widthDiv}px` }}
+                  className={`h-auto bg-red-500 ${classname}`}
+                >
+                  <h1>
+                    {item}
+                    {index}
+                  </h1>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute mt-40 ">
+        <button
+          className="rounded-lg border-2 px-9 border-blue-100"
+          onClick={onNextClick}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
