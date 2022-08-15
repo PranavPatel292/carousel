17b10ba7379f8457cdef9cadf2f011b7fb01a481
= <pranav.patel@exodigital.com.au>
2022-08-15 17:44:17 +1000

    added function getShowNNodes

--- src/LinkList.js
+++ src/LinkList.js
@@ -1,4 +1,4 @@
-class node {
+class Node {
   constructor(value) {
     self.value = value;
     self.next = null;
@@ -8,16 +8,16 @@ class node {
 
 const insertHelper = (value, arrayLength, currentIndex, head, temp) => {
   if (head === null) {
-    head = node(value);
+    head = Node(value);
     temp = head;
   } else if (currentIndex != arrayLength) {
-    temp.next = node;
-    node.prev = temp;
+    temp.next = Node;
+    Node.prev = temp;
     temp = temp.next;
   } else if (currentIndex === arrayLength) {
-    temp.next = node;
-    node.prev = temp;
-    node.next = head;
+    temp.next = Node;
+    Node.prev = temp;
+    Node.next = head;
   }
 };
 
@@ -33,3 +33,51 @@ const insert = (array) => {
 
   return head;
 };
+
+const getNextNNodes = (node, n) => {
+  let temp = [];
+
+  while (n !== 0) {
+    temp.push(node.value);
+    temp = temp.next;
+    n -= 1;
+  }
+};
+
+const getPrevNNodes = (node, n) => {
+  let temp = [];
+
+  while (n !== 0) {
+    temp.push(node.value);
+    temp = temp.prev;
+    n -= 1;
+  }
+};
+
+const getShowNNodes = (node, n, direction, prevNNodeStart, nextNNodeStart) => {
+  let temp = [];
+
+  if (direction === "right") {
+    prevNNodeStart = node;
+
+    while (n !== 0) {
+      temp.push(node.value);
+      temp = temp.next;
+      n -= 1;
+    }
+
+    nextNNodeStart = node;
+  } else if (direction === "left") {
+    nextNNodeStart = node; // we might change this to node.prev!
+    while (n !== 0) {
+      temp.push(temp.value);
+      temp = temp.prev;
+      n -= 1;
+    }
+
+    prevNNodeStart = node;
+  }
+
+  return [temp, prevNNodeStart, nextNNodeStart];
+};
+export { insert, getNextNNodes, getPrevNNodes, getShowNNodes };
